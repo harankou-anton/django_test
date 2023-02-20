d7e4d70e59d3d6490f2807157cbd34624d13e73d
Anton Harankou <harankou.anton@gmail.com>
2023-02-20 21:56:51 +0300

    add "products" app

---
+++ shop/products/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+import logging
+from django.http import HttpResponse
+
+logger = logging.getLogger(__name__)
+
+
+def index(request):
+    logger.info(request.GET, request.POST)
+    return HttpResponse("Shop index view")
