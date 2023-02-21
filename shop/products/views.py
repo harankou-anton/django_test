1801a962c5a32b984e223e64230ab29bed188d85
Anton Harankou <harankou.anton@gmail.com>
2023-02-21 22:48:24 +0300

    add "profile" app

--- shop/products/views.py
+++ shop/products/views.py
@@ -9,4 +9,12 @@ logger = logging.getLogger(__name__)
 
 def index(request):
     logger.info(request.GET, request.POST)
+    if request.GET.get("key") is not None:
+        logger.info(request.GET.get("key"))
+        return HttpResponse(f"Your GET query is 'key = {request.GET.get('key')}'")
     return HttpResponse("Shop index view")
+
+
+def profile(request):
+    logger.info(request.GET, request.POST)
+    return HttpResponse("Profile page")
