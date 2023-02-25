789f3c9ec9f855ddf137c271bd8d4d0852b505cb
Anton Harankou <harankou.anton@gmail.com>
2023-02-25 22:20:35 +0300

    add check for environment variable

--- shop/products/views.py
+++ shop/products/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -13,5 +15,10 @@ def index(request):
         logger.info(f'Custom var = {settings.MY_CUSTOM_VARAIBLE}')
         logger.info(f'Custom env var = {settings.MY_ENV_VARAIBLE}')
         logger.info(f"My param = {request.GET.get('param')}")
+
+    if int(os.getenv(key='FIRST_ENV_PARAM')) % 2 != 0:
+        logger.info(f'first environment variable is {os.getenv(key="SECOND_ENV_PARAM")}')
+    else:
+        logger.info(f'first environment variable is {os.getenv(key="THIRD_ENV_PARAM")}')
     return HttpResponse("Shop index view")
 
